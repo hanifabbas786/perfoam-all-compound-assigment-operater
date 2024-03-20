617b8c91463c71fdb7b8790aea244ac32fe18aca
dev_hanif <152361547+hanifabbas786@users.noreply.github.com>
2024-03-20 22:51:30 +0500

    Initial commit

---
+++ perfoam all compound assigment oprater on integer.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"enter a number:";
+	cin>>a;
+	a+=10;
+	cout<<"value of after a+=10:"<<a<<endl;
+	a-=10;
+	cout<<"value of after of a:"<<a<<endl;
+	
+
+	return 0;
+}
+
